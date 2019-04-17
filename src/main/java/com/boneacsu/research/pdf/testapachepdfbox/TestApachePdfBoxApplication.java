b7f888b65afde6a50ce06f76f6ee0d8f705ed377
boneacsu <bogdan.neacsu@hotmail.com>
2019-04-17 18:12:58 +0300

    fix .gitignore

---
+++ src/main/java/com/boneacsu/research/pdf/testapachepdfbox/TestApachePdfBoxApplication.java
@@ -0,0 +1,13 @@
+package com.boneacsu.research.pdf.testapachepdfbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApachePdfBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApachePdfBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boneacsu/research/pdf/testapachepdfbox/TestApachePdfBoxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boneacsu.research.pdf.testapachepdfbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApachePdfBoxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
